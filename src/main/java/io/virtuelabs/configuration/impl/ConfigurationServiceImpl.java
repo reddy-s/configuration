50b8bdd5aeb2640fa2ce71f867d5c0c6abb4efd7
Sangram Reddy <reddy.horcrux@gmail.com>
2019-06-28 16:30:55 +0100

    Simulating latency

--- src/main/java/io/virtuelabs/configuration/impl/ConfigurationServiceImpl.java
+++ src/main/java/io/virtuelabs/configuration/impl/ConfigurationServiceImpl.java
@@ -12,12 +12,15 @@ public class ConfigurationServiceImpl extends ConfigurationServiceGrpc.Configura
 
   private static final Logger LOGGER = Logger.getLogger(ConfigurationServiceGrpc.class.getName());
 
+  private static long addLatencyMs = Long.parseLong(System.getenv("IOT_CONFIGURATION_SERVICE_ADD_LATENCY"));
+
   @Override
   public void sendEvent(ConfigRequest request, StreamObserver<ConfigResponse> responseObserver) {
 
     LOGGER.log(Level.INFO, "Received request from DeviceService for device: {0}", request.getDeviceId());
     try {
       LOGGER.log(Level.INFO, "Processing request from DeviceService for device: {0}", request.getDeviceId());
+      Thread.sleep(addLatencyMs);
       responseObserver.onNext(
         ConfigResponse.newBuilder()
           .setBeatFrequency(10)
@@ -26,6 +29,8 @@ public class ConfigurationServiceImpl extends ConfigurationServiceGrpc.Configura
     } catch (RuntimeException e) {
       responseObserver.onError(e);
       throw e;
+    } catch (InterruptedException e) {
+      responseObserver.onError(e);
     }
 
     LOGGER.log(Level.INFO, "Responding to request from DeviceService for device: {0}", request.getDeviceId());
